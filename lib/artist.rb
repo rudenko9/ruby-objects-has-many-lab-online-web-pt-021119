552d1e6cc1ec4728db2410a6714e60b2d2f10336
Nataliya R <n.rudenko@gmail.com>
2019-03-19 14:16:48 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist 
+   def initialize(name)
+     @name = name
+    @songs = []
+   end 
+   
+  attr_accessor :name
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+ 
+  def songs
+    @songs
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+  
+  
+end 
