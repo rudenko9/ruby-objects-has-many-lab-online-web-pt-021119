39b4b2aaef2ef88bfa54c7e7247c0d274099a351
Nataliya R <n.rudenko@gmail.com>
2019-03-19 16:10:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,8 +15,8 @@ class Author
     @post
   end
   
-  def add_post_by_title(name)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @post << post
     post.author = self
   end
